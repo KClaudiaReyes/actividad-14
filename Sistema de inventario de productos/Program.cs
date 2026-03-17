95955352d19b1d982c3b972b13227507a1d2aaac
clauR <creyesccf@gmail.com>
2026-03-17 19:28:57 +0100

    creacion de class y list

---
+++ Sistema de inventario de productos/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+
+List <Producto> producto = new List <Producto> ();
+
+
+
+
+
+
+class Producto 
+{
+    public string Nombre;
+    public double precio;
+    public int Cantidad;
+
+    public double CalcularValorINventario()
+    {return precio * Cantidad;}
+
+    public string EvaluarEstock()
+    {
+        if (Cantidad == 0) return "SIN EXISTENCIA";
+        else if (Cantidad == 10) return " Stock Bajo";
+        else return "Stock Suficiente";
+    }
+
+    public void MostarDatos()
+    {
+        Console.WriteLine($"Nombre: {Nombre} |Precio:Q {precio}:F2 | cantidad: {Cantidad} ");
+    }
+}
