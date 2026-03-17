ce04d1d1b520ace5fa8eebaaa204a9344a4aceb6
clauR <creyesccf@gmail.com>
2026-03-17 19:49:51 +0100

    finalizado

--- Sistema de inventario de productos/Program.cs
+++ Sistema de inventario de productos/Program.cs
@@ -5,11 +5,30 @@ using System.Collections.Generic;
 
 List <Producto> producto = new List <Producto> ();
 
+Console.WriteLine("cuantos Productos desea registrar? ");
+int n = int.Parse (Console.ReadLine ());
 
+for(int i = 0; i < n; i++)
+{
+    Producto p = new Producto ();
+    Console.WriteLine($"\n  Producto {i + 1}");
+    Console.WriteLine("Nombre: "); p.Nombre = Console.ReadLine();
+    Console.WriteLine("Precio: "); p.precio = double.Parse (Console.ReadLine());
+    Console.WriteLine("Cantidad: "); p.Cantidad = int.Parse (Console.ReadLine());
+    producto.Add (p);
+}
 
-
-
-
+double InventarioTotal = 0;
+Producto MasCostoso = producto[0];
+Console.WriteLine("\n INVENTARIO ");
+foreach(Producto p in producto)
+{
+    InventarioTotal += p.CalcularValorINventario();
+    if (p.precio > MasCostoso.precio)
+    { MasCostoso = p; }
+}
+Console.WriteLine($"Valor Total del inventario:Q {InventarioTotal}:F2");
+Console.WriteLine("Producto con mayor precio: "); MasCostoso.MostarDatos();
 class Producto 
 {
     public string Nombre;
