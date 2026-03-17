652994c29df1652e4260996a2c4c382ea266a2f6
clauR <creyesccf@gmail.com>
2026-03-17 19:16:21 +0100

    finalizado

--- Sistema de registro de estudiantes/Program.cs
+++ Sistema de registro de estudiantes/Program.cs
@@ -1,19 +1,56 @@
 ﻿using System;
 using System.Collections.Generic;
 
-class  Estudiante
+
+
+
+List<Estudiante> estudiantes = new List<Estudiante>();
+
+
+Console.WriteLine("cuantos estidiantes desea agregar? ");
+int n = int.Parse(Console.ReadLine());
+
+for (int i = 0; i < n; i++)
 {
-   public string nombre;
-  public  double nota1;
-  public  double nota2;
-   public double nota3;
+    Estudiante e = new Estudiante();
+    Console.WriteLine($"Estudiante{i + 1} ");
+    Console.WriteLine("Nombre: "); e.Nombre = Console.ReadLine();
+    Console.WriteLine("Nota 1: "); e.nota1 = double.Parse(Console.ReadLine());
+    Console.WriteLine("Nota 2: "); e.nota2 = double.Parse(Console.ReadLine());
+    Console.WriteLine("Nota 3: "); e.nota3 = double.Parse(Console.ReadLine());
+    estudiantes.Add(e);
+}
+
+double SumarPromedios = 0;
+Estudiante mejor = estudiantes[0];
+Console.WriteLine(" \n LISTADO DE ESTUDIANTES ");
+foreach (Estudiante e in estudiantes)
+{
+    e.MostarDatos();
+    SumarPromedios += e.CalcularPromedio();
+    if (e.CalcularPromedio() > mejor.CalcularPromedio())
+    { mejor = e; }
+
+}
+Console.WriteLine($"Promedio general del Grupo: {SumarPromedios / estudiantes.Count:F2} ");
+Console.WriteLine("Mejor estudainte: "); mejor.MostarDatos();
+
+
+
+
+class Estudiante
+{
+    public string Nombre;
+    public double nota1;
+    public double nota2;
+    public double nota3;
 
     public double CalcularPromedio()
     {
         return (nota1 + nota2 + nota3) / 3;
     }
 
-    public string ObtenerEstado ()
+    public string ObtenerEstado()
     {
         if (CalcularPromedio() >= 61)
             return "Aprobado";
@@ -22,30 +59,7 @@ class  Estudiante
 
     public void MostarDatos()
     {
-        Console.WriteLine($"Nombre: {nombre} | Promedio: {CalcularPromedio():F2} | Estado: {ObtenerEstado()}");
+        Console.WriteLine($"Nombre: {Nombre} | Promedio: {CalcularPromedio():F2} | Estado: {ObtenerEstado()}");
     }
 
 }
-
-List <Estudiante> estudiantes = new List<Estudiante>();
-
-Console.WriteLine("cuantos estidiantes desea agregar? ");
-int n = int.Parse(Console.ReadLine());
-
-for (int i = 0;i > n; i++)
-{
-    Estudiante e = new Estudiante();
-    Console.WriteLine($"Estudiante{i + 1} ");
-    Console.WriteLine("Nombre: "); e.nombre = Console.ReadLine();
-    Console.WriteLine("Nota 1: "); e.nota1 = double.Parse(Console.ReadLine());
-    Console.WriteLine("Nota 2: "); e.nota2 = double.Parse(Console.ReadLine());
-    Console.WriteLine("Nota 3: "); e.nota3 = double.Parse(Console.ReadLine());
-    estudiantes.Add(e);
-}
-
-double SumarPromedio = 0;
-Estudiante mejor = estudiantes[0];
-Console.WriteLine(" \n LISTADO DE ESTUDIANTES ");
-
-
-
