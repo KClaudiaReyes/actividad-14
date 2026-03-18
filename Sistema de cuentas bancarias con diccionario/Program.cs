2eb0123c0bdbbd57d016c1c65cf8857205b60609
clauR <creyesccf@gmail.com>
2026-03-19 00:13:55 +0100

    class y lista con for

---
+++ Sistema de cuentas bancarias con diccionario/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+
+List<Cuenta> cuentas = new List<Cuenta>();
+Console.WriteLine("Cuantas Cuentas Desea Ingresar? :");
+int n = int.Parse(Console.ReadLine());
+
+for (int i = 0; i < n; i++)
+{
+    Cuenta c = new Cuenta();
+    Console.WriteLine($"Cuenta {i + 1}");
+    Console.WriteLine("Titular: "); c.Titular = Console.ReadKey();
+    Console.WriteLine("Saldo Inicial: "); c.Saldo = double.Parse(Console.ReadLine());
+    Console.WriteLine("Monto a Depositar: ");
+    double deposito = double.Parse(Console.ReadLine());
+    Console.Write("Monto a Retirar");
+    double retiro = double.Parse(Console.ReadLine());
+    bool retiroExitoso = c.Retirar(retiro);
+    if(!retiroExitoso)
+    {
+        Console.WriteLine("No se puede realizar el retiro por saldo insuficiente");
+    }
+}
+class Cuenta
+{
+    public string Titular;
+    public double Saldo;
+
+    public void Depositar(double monto) 
+    {   Saldo += monto; }
+
+    public bool Retirar(double monto)
+    {
+        if(monto <= Saldo) 
+        { Saldo -= monto;
+            return true; 
+        }
+        return false;
+    }
+
+    public void MostrarDatos()
+    {
+        Console.WriteLine($"Titular: {Titular} | Saldo: Q{Saldo:F2}");
+    }
+
+
+}
