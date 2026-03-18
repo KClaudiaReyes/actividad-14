4056ae027d00053a9b2264d326ae51a85077778e
clauR <creyesccf@gmail.com>
2026-03-18 23:09:56 +0100

    class

---
+++ Sistema de productos por código/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+
+Dictionary<string, Producto> producto = new Dictionary<string, Producto>();
+Console.WriteLine("Cuantos Productos desea registrar? : ");
+int n = int.Parse(Console.ReadLine());
+for (int i = 0; i < n; i++)
+{
+    
+}
+
+
+class Producto
+{
+    public string Nombre;
+    public double precio;
+    public int Stock;
+
+    public string EstadoStock()
+    {
+        if (Stock == 0) return "AGOTADO";
+        else if (Stock <= 10) return "BAJO";
+        else return "NORMAL";
+    }
+
+    public void MostrarDatos()
+    {
+        Console.WriteLine($"nombre: {Nombre}  | Precio: Q{precio: F2} | Stock: {Stock} | Estado: {EstadoStock()}");
+
+    }
+
+}
