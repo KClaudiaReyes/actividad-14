3d784d9f73c4229edfdc3f9890ad911a0630baf9
clauR <creyesccf@gmail.com>
2026-03-19 01:05:03 +0100

    ejrcico 10 class creada

---
+++ Sistema de pedidos de clientes/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Pedido
+{
+    public string Cliente;
+    public string Producto;
+    public int cantidad;
+    public double PrecioUnitario;
+
+    public double CalcularSubtotal()
+    {
+        return cantidad * PrecioUnitario;
+    }
+    public double CalcularEnvio()
+    {
+        if (CalcularSubtotal() >= 300) return 0;
+        else return 25;
+    }
+    public double CalcularTotal() { return CalcularSubtotal() + CalcularEnvio(); }
+    public void MostrarDatos()
+    {
+        Console.WriteLine($"Cliente: {Cliente} | Producto: {Producto} | Cantidad: {cantidad} | Subtotal: {CalcularSubtotal()} | Envio: {CalcularEnvio():F2} | TOtal Final: {CalcularTotal():F2}");
+    }
+
+}
